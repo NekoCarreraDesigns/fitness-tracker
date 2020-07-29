a98dc296c92835e91876968e182ef8b0269ab6d2
NekoCarreraDesigns <denvermaas@gmail.com>
2020-07-29 02:48:22 -0600

    wrote the routes for the app

--- controllers/fitness_controller.js
+++ controllers/fitness_controller.js
@@ -1,11 +1,67 @@
 const express = require("express");
-const db = require("./models")
+const db = require("./models");
+const { Router } = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    db.workout.find({}, (req, data) => {
-        res.json(data)
-    })
-})
-module.exports = routes;
+app.get("/api/workouts", (req, res) => {
+    db.Exercise.find({})
+        .then((dbExercise) => {
+            res.json(dbExercise);
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(404)
+            } else {
+                console.log(200)
+            };
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", (req, res) => {
+    db.Exercise.create(req.body)
+        .then((dbExercise) => {
+            res.json(dbExercise);
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(404);
+            } else {
+                console.log(200);
+            };
+            res.json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    db.Exercise.updateOne({ _id: params }, { $push: { exercises: body } })
+        .then((dbExercise) => {
+            res.json(dbExercise);
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(404);
+            } else {
+                console.log(200)
+            };
+            res.json(err)
+        });
+});
+
+router.get("/api/workout/range", (req, res) => {
+    db.Exercise.find({})
+        .then((range) => {
+            res.json(range);
+        })
+        .catch((err) => {
+            if (err) {
+                console.log(404);
+            } else {
+                console.log(200);
+            }
+            res.json(err);
+        });
+});
+
+module.exports = router;
